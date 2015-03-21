4e18ce8ad256f5059c80218c8781137a89d7e1d1
Markus Fischer <markus.fischer@exxeta.de>
2015-03-21 14:35:54 +0100

    adapted logging to slf4j implemented first version of table logic

--- src/org/eclipse/iot/hotdesking/activator/Activator.java
+++ src/org/eclipse/iot/hotdesking/activator/Activator.java
@@ -1,5 +1,9 @@
 package org.eclipse.iot.hotdesking.activator;
 
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +12,11 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalMultipurpose;
 import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
 
 public class Activator {
 
@@ -18,26 +24,55 @@ public class Activator {
 
     private GpioPinDigitalMultipurpose motionSensor;
 
+    private volatile boolean motionTriggered = false;
+
+    private ScheduledThreadPoolExecutor threadPoolExecutor;
+
+    private ScheduledFuture<?> handle;
+
     private Logger logger = LoggerFactory.getLogger(Activator.class);
 
-    private static final String APP_ID = "com.exxeta.motion.hello_world";
+    private static final String APP_ID = "org.eclipse.iot.hotdesking";
 
     protected void activate(ComponentContext componentContext) {
-        logger.info("Bundle " + APP_ID + " tries to start...");
+        logger.info("Bundle {} tries to start...", APP_ID);
         gpioController = GpioFactory.getInstance();
 
         motionSensor = gpioController.provisionDigitalMultipurposePin(RaspiPin.GPIO_01, PinMode.DIGITAL_INPUT);
         motionSensor.setShutdownOptions(true);
 
+        GpioPinDigitalMultipurpose led = gpioController.provisionDigitalMultipurposePin(RaspiPin.GPIO_04, PinMode.DIGITAL_OUTPUT);
+
+        motionSensor.addTrigger(new GpioSyncStateTrigger(led));
+
+        threadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+
+        handle = threadPoolExecutor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                if (motionTriggered) {
+                    logger.info("table is occupied for now");
+                } else {
+                    logger.info("table is free");
+                }
+                setMotionTriggeredState(false);
+            }
+        }, 0, 30, TimeUnit.SECONDS);
+
         motionSensor.addListener(new GpioPinListenerDigital() {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                logger.info("Pin state: {}", event.getState());
+                if (event.getState() == PinState.HIGH) {
+                    setMotionTriggeredState(true);
+                }
             }
         });
 
-        logger.info("Bundle " + APP_ID + " has started!");
-        logger.debug(APP_ID + ": This is a debug message.");
+        logger.info("Bundle {} has started!", APP_ID);
+    }
+
+    private void setMotionTriggeredState(boolean newState) {
+        motionTriggered = newState;
     }
 
     protected void deactivate(ComponentContext componentContext) {
@@ -45,8 +80,11 @@ public class Activator {
             gpioController.unexportAll();
             gpioController.shutdown();
         }
+        if (handle != null) {
+            handle.cancel(true);
+        }
 
-        logger.info("Bundle " + APP_ID + " has stopped!");
+        logger.info("Bundle {} has stopped!", APP_ID);
     }
 
 }
