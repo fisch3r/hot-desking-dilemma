292b6f9dfeeac749ac29008263ec8daabe13f49c
Nils Eckert <nils.eckert@eckert-partner.it>
2015-03-23 15:21:34 +0100

    Connect to MQTT, show current state

--- webclient/app/app.js
+++ webclient/app/app.js
@@ -4,6 +4,51 @@
 var hotdesk = angular.module('hotdesk', [ 'ngRoute', 'hotdesk.view1',
 		'hotdesk.view2', 'hotdesk.version', 'ui.bootstrap' ]);
 
+hotdesk.run(function($rootScope, StateService) {
+	var mqttClient = new Paho.MQTT.Client('192.168.1.10', Number(9001),
+			"clientId");
+
+	mqttClient.startTrace();
+	mqttClient.onConnectionLost = onConnectionLost;
+	mqttClient.onMessageArrived = onMessageArrived;
+	mqttClient.connect({
+		onSuccess : onConnect,
+		userName : "user",
+		password : "hot-desk"
+	});
+
+	// called when the client connects
+	function onConnect() {
+		// Once a connection has been made, make a subscription and send
+		// a message.
+		console.log("onConnect");
+		mqttClient.subscribe("hot-desks/#");
+	}
+
+	// called when the client loses its connection
+	function onConnectionLost(responseObject) {
+		if (responseObject.errorCode !== 0) {
+			console.log("onConnectionLost:" + responseObject.errorMessage);
+		}
+	}
+
+	// called when a message arrives
+	function onMessageArrived(message) {
+		console.log("onMessageArrived:" + message.payloadString);
+
+		var payload = JSON.parse(message.payloadString);
+		StateService.state[payload.name] = payload;
+		
+		$rootScope.$broadcast('tableUpdate');
+	}
+
+	$rootScope.mqttClient = mqttClient;
+});
+
+hotdesk.factory('StateService', function() {
+	return { state : {} };
+});
+
 hotdesk.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.otherwise({
 		redirectTo : '/view1'
--- webclient/app/view1/view1.js
+++ webclient/app/view1/view1.js
@@ -1,14 +1,20 @@
 'use strict';
 
-angular.module('hotdesk.view1', ['ngRoute'])
+angular.module('hotdesk.view1', [ 'ngRoute' ])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/view1', {
+		templateUrl : 'view1/view1.html',
+		controller : 'View1Ctrl'
+	});
+} ])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope, StateService) {
+	$scope.tables = StateService.state;
 
-}]);
+	$scope.$on('tableUpdate', function(event) {
+		$scope.tables = StateService.state;
+		$scope.latestUpdate = new Date();
+		$scope.$apply();
+	});
+});
